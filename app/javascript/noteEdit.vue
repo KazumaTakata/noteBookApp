d9211fcf615919f9780991176ac857028171a416
Kazuma Takata <neilbuzztakata@gmail.com>
2018-04-17 00:57:22 +0900

    add audioinput

--- app/javascript/noteEdit.vue
+++ app/javascript/noteEdit.vue
@@ -15,6 +15,12 @@
       </div>
     </div>
   </div>
+  <div class="audioContainer">
+    <button v-on:click="startRecord" class="recordButton" type="button" name="button">{{recordButtonString}}</button>
+    <div class="">
+
+    </div>
+  </div>
   </div>
 </template>
 
@@ -22,15 +28,72 @@
 import axios from "axios";
 import katex from  "katex";
 import showdown from "showdown";
+import Vue from "vue"
+
 let converter = new showdown.Converter()
 export default {
+
   data: function () {
     return {
       markdownData: this.$store.state.classData[this.$route.params.noteId[0]][this.$route.params.noteId[1]].note[this.$route.params.listId].markdown,
-      htmlCode: this.compileMarkdownHelp(this.$store.state.classData[this.$route.params.noteId[0]][this.$route.params.noteId[1]].note[this.$route.params.listId].markdown)
+      htmlCode: this.compileMarkdownHelp(this.$store.state.classData[this.$route.params.noteId[0]][this.$route.params.noteId[1]].note[this.$route.params.listId].markdown),
+      audioData: { data: [], sampleRate: 44100, numberOfChannels:1 , length: 8192},
+      recordState: false,
+      recordButtonString: "start"
     }
   },
   methods:{
+    startRecord: function(){
+      console.log("startRecord")
+      navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia;
+      let constraints = {audio: true, video: false}
+
+      function errorCallback(error) {
+        console.log("navigator.getUserMedia error:", error);
+      }
+
+      if (!this.recordState){
+        this.recordButtonString = "recording"
+      } else {
+        this.recordButtonString = "start"
+      }
+      this.recordState = !this.recordState
+
+
+      console.log(this.iconClass)
+      navigator.getUserMedia(constraints, this.successCallback, errorCallback);
+    },
+    successCallback: function(stream){
+      window.stream = stream;
+      console.log("success")
+
+      var context = new AudioContext();
+      var analyser = context.createAnalyser();
+
+      var source = context.createMediaStreamSource(stream);
+      // source.connect(analyser);
+      // analyser.connect(context.destination)
+      //
+      // analyser.fftSize = 2048;
+      // var bufferLength = analyser.frequencyBinCount;
+      // var dataArray = new Uint8Array(bufferLength);
+      // analyser.getByteFrequencyData(dataArray);
+
+      var processor = context.createScriptProcessor(1024*8, 1, 1);
+
+      source.connect(processor);
+      processor.connect(context.destination);
+      processor.onaudioprocess = this.onaudioprocess
+
+    },
+
+    onaudioprocess: function(e){
+      // console.log(dataArray)
+      let inputData = e.inputBuffer.getChannelData(0)
+      let outputData = e.outputBuffer.getChannelData(0)
+      this.audioData.data.push(...inputData)
+    },
+
     getRoute: function(){
       return this.$route.path
     },
@@ -95,6 +158,13 @@ a{
   font-size: 30px;
 }
 
+
+.recordButton{
+  background: transparent;
+  border: 1px solid black;
+  color: black;
+}
+
 .htmlOutput{
   overflow: scroll;
   width: 350px;
@@ -119,6 +189,13 @@ a{
   margin: auto;
   margin-top: 30px;
 }
+
+.audioContainer{
+  margin: auto;
+  width: 700px;
+  margin-top: 50px;
+}
+
 
 .markdownEditor{
   height: 100%;
